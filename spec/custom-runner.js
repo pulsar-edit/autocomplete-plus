884287d73ede5f7455ba09116b71b6939efc611a
Maurício Szabo <mauricio.szabo@gmail.com>
2023-01-14 18:06:00 -0300

    Removed old `.and.callFake` calls

--- spec/custom-runner.js
+++ spec/custom-runner.js
@@ -1,8 +1,8 @@
-const { createRunner } = require('atom-jasmine3-test-runner')
-
-const options = {
-  specHelper: true,
-  silentInstallation: true
-}
-
-module.exports = createRunner(options)
+// const { createRunner } = require('atom-jasmine3-test-runner')
+//
+// const options = {
+//   specHelper: true,
+//   silentInstallation: true
+// }
+//
+// module.exports = createRunner(options)
